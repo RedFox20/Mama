530a22dd0be1f1b04ea7fe64f4a593a92d5c323c
Jorma <jorma.rebane@gmail.com>
2018-03-11 22:49:36 +0200

    Added example_remote as a submodule

--- test/example_consumer/mamafile.py
+++ test/example_consumer/mamafile.py
@@ -4,9 +4,4 @@ class ExampleConsumer(mama.BuildTarget):
     workspace = 'wolf3d'
     def dependencies(self):
         self.add_local('ExampleLibrary', '../example_library')
-
-    def build(self):
-        pass
-
-    def package(self):
-        pass
+        self.add_git('ExampleRemote', 'https://github.com/RedFox20/MamaExampleRemote.git')
