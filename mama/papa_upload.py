f376436dd460648e2f18df904959fbc25506eee0
Jorma Rebane <jorma.rebane@gmail.com>
2024-03-24 23:42:05 +0200

    fix: use compression for zip archiving

--- mama/papa_upload.py
+++ mama/papa_upload.py
@@ -53,7 +53,9 @@ def papa_upload_to(target:BuildTarget, package_full_path:str):
     papa = PapaFileInfo(papa_file)
     # create a zip archive with papa.includes, papa.libs and papa.assets
     temp_archive = archive_path + '.tmp'
-    with zipfile.ZipFile(temp_archive, 'w') as zip:
+    with zipfile.ZipFile(temp_archive, 'w',
+                         compression=zipfile.ZIP_DEFLATED,
+                         compresslevel=8) as zip:
         zip.write(papa_file, 'papa.txt') # always add the main manifest file
         for include in papa.includes:
             if config.verbose: console(f'      adding {include}')
