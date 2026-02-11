9ce501d43e306372c83203c3c9c674a8d8ee95bc
BatteredBunny <ayes2022@protonmail.com>
2026-02-11 12:22:29 +0200

    refactor: Clean up test_git_pinning

--- tests/test_git_pinning/test_git_pinning.py
+++ tests/test_git_pinning/test_git_pinning.py
@@ -4,15 +4,15 @@ import sys
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
 from testutils import shell_exec, file_contains
 
-def get_dep_path(dep_name):
-    return os.path.join('packages', dep_name, dep_name)
+def remote_file_contains(dep_name, text):
+    return file_contains(os.path.join('packages', dep_name, dep_name, 'remote.h'), text)
 
 def test_git_pinning():
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
     shell_exec("mama clean")
 
-    assert not file_contains(os.path.join(get_dep_path('ExampleRemote'), 'remote.h'), 'REMOTE_VERSION')
-    assert file_contains(os.path.join(get_dep_path('ExampleRemote2'), 'remote.h'), 'REMOTE_VERSION 2')
-    assert not file_contains(os.path.join(get_dep_path('ExampleRemote3'), 'remote.h'), 'REMOTE_VERSION')
-    assert file_contains(os.path.join(get_dep_path('ExampleRemote4'), 'remote.h'), 'REMOTE_VERSION 2')
+    assert not remote_file_contains('ExampleRemote', 'REMOTE_VERSION')
+    assert remote_file_contains('ExampleRemote2', 'REMOTE_VERSION 2')
+    assert not remote_file_contains('ExampleRemote3', 'REMOTE_VERSION')
+    assert remote_file_contains('ExampleRemote4', 'REMOTE_VERSION 2')
