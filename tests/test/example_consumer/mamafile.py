a31028f91113d133b756b2596f54451a7ab8e9a9
Jorma Rebane <jorma.rebane@krattworks.com>
2026-02-12 13:40:36 +0200

    feature: support commit pinning via tags

--- tests/test/example_consumer/mamafile.py
+++ tests/test/example_consumer/mamafile.py
@@ -1,11 +1,15 @@
 import mama
 
 class ExampleConsumer(mama.BuildTarget):
-    workspace = 'wolf3d'
+    workspace = 'packages'
+
+    def init(self):
+        self.prefer_gcc()
 
     def dependencies(self):
         self.add_local('ExampleLibrary', '../example_library')
-        self.add_git('ExampleRemote', 'https://github.com/RedFox20/MamaExampleRemote.git')
+        self.add_git('ExampleRemote', 'https://github.com/RedFox20/MamaExampleRemote.git',
+                     git_tag='4acd9052f27a459314651dd485ae8fa79a04d49d')
 
     # optional: pre-build configuration step
     def configure(self):
