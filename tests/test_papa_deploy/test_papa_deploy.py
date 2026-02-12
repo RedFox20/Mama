fc95f6041d91289f3a66223d351bb3ec6747b151
BatteredBunny <ayes2022@protonmail.com>
2026-02-12 12:25:38 +0200

    review changes

--- tests/test_papa_deploy/test_papa_deploy.py
+++ tests/test_papa_deploy/test_papa_deploy.py
@@ -1,4 +1,4 @@
-from testutils import init, is_linux, shell_exec, file_exists, is_windows
+from testutils import init, shell_exec, file_exists, executable_extension, native_platform_name
 
 # Generic test to verify basic build and deploy functions work
 def test_papa_deploy():
@@ -7,18 +7,5 @@ def test_papa_deploy():
     shell_exec("mama build")
     shell_exec("mama deploy")
 
-    if is_windows():
-        extension = '.exe'
-    else:
-        extension = ''
-
-    assert file_exists(f'bin/ExampleConsumer{extension}'), "Deployed executable not found"
-
-    if is_windows():
-        platform_name = 'windows'
-    elif is_linux():
-        platform_name = 'linux'
-    else:
-        raise Exception("Unsupported platform")
-
-    assert file_exists(f'packages/ExampleConsumer/{platform_name}/deploy/ExampleConsumer/papa.txt'), "Deployed papa.txt not found for dependency"
+    assert file_exists(f'bin/ExampleConsumer{executable_extension()}'), "Deployed executable not found"
+    assert file_exists(f'packages/ExampleConsumer/{native_platform_name()}/deploy/ExampleConsumer/papa.txt'), "Deployed papa.txt not found for dependency"
--- tests/testutils.py
+++ tests/testutils.py
@@ -35,6 +35,40 @@ def is_windows() -> bool:
 def is_linux() -> bool:
     return os.name == 'posix' and sys.platform != 'darwin'
 
+def is_macos() -> bool:
+    return sys.platform == 'darwin'
+
+def executable_extension() -> str:
+    if is_windows():
+        return '.exe'
+
+    return ''
+
+def static_library_extension() -> str:
+    if is_windows():
+        return '.lib'
+    else:
+        return '.a'
+
+def dynamic_library_extension() -> str:
+    if is_windows():
+        return '.dll'
+    elif is_macos():
+        return '.dylib'
+    else:
+        return '.so'
+
+# Excludes for example android
+def native_platform_name() -> str:
+    if is_windows():
+        return 'windows'
+    elif is_linux():
+        return 'linux'
+    elif is_macos():
+        return 'macos'
+    else:
+        raise Exception("Unsupported platform")
+
 def onerror(func, path, _):
     import stat
     if not os.access(path, os.W_OK):
